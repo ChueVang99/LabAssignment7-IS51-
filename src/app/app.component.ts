3f97116f64ff663340a03bdb0672cfa4a3fb9d44
ChueVang99 <chuevang10@gmail.com>
2019-12-03 13:40:16 -0800

    LabAssingment7

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ToastService } from './toast/toast.service';
+import { ToastComponent } from './toast/toast/toast.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'toast-app';
+  toastTypes: Array<string> = [];
+  constructor(private toastService: ToastService) {
+    this.toastTypes = ['success', 'info', 'warning', 'danger'];
+  }
+
+  // success, info, warning, danger
+  showToast() {
+    const rand = Math.floor(Math.random() * 4);
+    const toastType = this.toastTypes[rand];
+    const toastMessage = 'Hi This is a message my random is: ' + rand;
+    const duration = 4000;
+    this.toastService.showToast(toastType, toastMessage, duration);
+  }
+
+}
